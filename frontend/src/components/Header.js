344078b8bb2d2a3ab605ae575328f193164a2d9d
lag-packet <Satarhassni0@gmail.com>
2024-01-15 23:13:01 -0500

    seperated homepage content into components.

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,13 @@
+// Header.js
+import React from "react";
+
+function Header() {
+  return (
+    <header className="text-center my-4">
+      <h1 className="text-4xl font-bold text-gray-800">Finance Tracker</h1>
+      <p className="text-gray-600">Homepage</p>
+    </header>
+  );
+}
+
+export default Header;

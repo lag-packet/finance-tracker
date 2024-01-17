f35a2a19d97a1288f77a5401e4a9cb34b5a31bb9
lag-packet <Satarhassni0@gmail.com>
2024-01-16 20:41:33 -0500

    Removed boiler code and updated
    changed header to no longer have boiler code. Also changed it to have logo included from previous removal of HomePage modularization.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,11 @@
 // Header.js
 import React from "react";
+import logo from "../imgs/logo.png";
 
 function Header() {
   return (
     <header className="text-center my-4">
-      <h1 className="text-4xl font-bold text-gray-800">Finance Tracker</h1>
-      <p className="text-gray-600">Homepage</p>
+      <img className="mx-auto" src={logo} width="200px" height="auto"></img>
     </header>
   );
 }

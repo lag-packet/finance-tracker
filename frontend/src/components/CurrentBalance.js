65b728b65b71743edf836cc1c794cf89bc305299
lag-packet <Satarhassni0@gmail.com>
2024-01-16 20:58:08 -0500

    added logic to Current Balance component
    added conditional rendering to enable user to set the balance when clicking on the component. This now changes it to a input field if user interacts with it and paragraph when done

--- frontend/src/components/CurrentBalance.js
+++ frontend/src/components/CurrentBalance.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useState } from "react";
 
-function CurrentBalance () {
-  // Assume balance is fetched or calculated
-  const balance = 1000;
+function CurrentBalance() {
+  const [balance, setBalance] = useState(0);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleBlur = (e) => {
+    //update local storage here.
+    setIsEditing(false);
+  }
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      //update local storage here
+      setIsEditing(false);
+    }
+  }
 
   return (
     <div className="bg-white shadow-md rounded-lg p-4">
       <h3 className="font-semibold text-lg text-gray-800">Current Balance</h3>
-      <p className="text-gray-600">${balance}</p>
+      {isEditing ? (
+        <input
+          type="number"
+          value={balance}
+          onBlur={handleBlur}
+          onKeyDown={handleKeyDown}
+          onChange={(e) => setBalance(e.target.value)}
+          className="text-gray-600 p-1 border-2 border-gray-300 rounded"
+          autoFocus
+        />
+      ) : (
+        <p className="text-gray-600" onClick={() => setIsEditing(true)}>
+          ${balance}
+        </p>
+      )}
     </div>
   );
 }
